98329fb1f1f3a850a373a368cc4d493b70d7ea14
sonnhfit <sonnhfit@gmail.com>
2022-05-31 02:44:19 +0700

    edit name doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'deepair'
+project = 'Deepair'
 copyright = '2022, Son Nguyen Huu'
 author = 'Son Nguyen Huu'
 
@@ -28,6 +28,7 @@ author = 'Son Nguyen Huu'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
