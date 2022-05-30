7f29761bb4a9e760136ed40fe51fb504e28c2b57
sonnhfit <sonnhfit@gmail.com>
2022-05-31 02:22:41 +0700

    init setup file

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+
+# load the README file and use it as the long_description for PyPI
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+# package configuration - for reference see:
+# https://setuptools.readthedocs.io/en/latest/setuptools.html#id9
+setup(
+    name="Deepair",
+    description="PyTorch implementations of Deep reinforcement learning algorithms.",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    version=0.1,
+    author="Son Nguyen Huu",
+    author_email="sonnhfit@gmail.com",
+    url="https://github.com/sonnhfit/deepair",
+    packages=[],
+    include_package_data=True,
+    python_requires=">=3.7.*",
+    install_requires=['numpy', 'requests'],
+    license="MIT",
+    zip_safe=False,
+    entry_points={
+        'console_scripts': ['py-package-template=py_pkg.entry_points:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords='reinforcement-learning-algorithms reinforcement-learning machine-learning deep rl'
+)
