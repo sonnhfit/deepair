3efeb6a1ee823d87cf777290cc2cc42802545003
sonnhfit <sonnhfit@gmail.com>
2022-05-31 16:09:20 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,6 @@ author = 'Son Nguyen Huu'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
